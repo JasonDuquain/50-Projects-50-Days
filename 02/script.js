9634e0495885274e00973db7f400f52237de17f0
Jason Duquain <jduquain@gmail.com>
2020-12-28 14:16:01 -0500

    day 02 updated to use svg - last commit has day 02 complete with divs

--- 02/script.js
+++ 02/script.js
@@ -1,36 +1,47 @@
 
 
 let nums = document.querySelectorAll('.num');
+let lines = document.querySelectorAll('line');
 let prev = document.querySelector('button:first-of-type');
 let next = document.querySelector('button:last-of-type');
 
+/// circle length 62.43
+console.log(document.querySelector('circle:first-of-type').getBBox())
+
 next.addEventListener('click', (e) => {
-  if (!nums[0].classList.contains('active')) {
-    nums[0].classList.add('active');
-    nums[1].classList.add('blue')
+  if (!nums[3].classList.contains('blue')) {
+    nums[3].classList.add('blue');
+    nums[4].classList.add('blue');
+    lines[1].classList.add('blue')
     prev.classList.add('enabled');
-  } else if (nums[0].classList.contains('active') && !nums[1].classList.contains('active')) {
-    nums[1].classList.add('active');
-    nums[2].classList.add('blue')
+  } else if (nums[3].classList.contains('blue') && !nums[6].classList.contains('blue')) {
+    nums[6].classList.add('blue');
+    nums[7].classList.add('blue');
+    lines[3].classList.add('blue')
     prev.classList.add('enabled');
-  } else if (nums[0].classList.contains('active') && nums[1].classList.contains('active') && !nums[2].classList.contains('active')) {
-    nums[2].classList.add('active');
-    nums[3].classList.add('blue')
+  } else if (nums[6].classList.contains('blue') && !nums[9].classList.contains('blue')) {
+    nums[9].classList.add('blue');
+    nums[10].classList.add('blue');
+    lines[5].classList.add('blue')
     next.classList.remove('enabled');
   }
 })
 
 prev.addEventListener('click', (e) => {
-  if (nums[0].classList.contains('active') && nums[1].classList.contains('active') && nums[2].classList.contains('active')) {
-    nums[2].classList.remove('active');
-    nums[3].classList.remove('blue')
-    next.classList.remove('enabled');
-  } else if (nums[0].classList.contains('active') && nums[1].classList.contains('active')) {
-    nums[1].classList.remove('active');
-    nums[2].classList.remove('blue')
-  } else if (nums[0].classList.contains('active')) {
-    nums[0].classList.remove('active');
-    nums[1].classList.remove('blue')
+  if (nums[9].classList.contains('blue')) {
+    nums[9].classList.remove('blue');
+    nums[10].classList.remove('blue');
+    lines[5].classList.remove('blue')
+    next.classList.add('enabled');
+  } else if (nums[6].classList.contains('blue')) {
+    nums[6].classList.remove('blue');
+    nums[7].classList.remove('blue');
+    lines[3].classList.remove('blue');
+    next.classList.add('enabled');
+  } else if (nums[3].classList.contains('blue')) {
+    nums[3].classList.remove('blue');
+    nums[4].classList.remove('blue');
+    lines[1].classList.remove('blue')
     prev.classList.remove('enabled');
     next.classList.add('enabled');
   }
